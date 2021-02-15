aee45fd1754ebd9b332cce4d9cd96fd7c0cfd132
Volodia <vovashishko253@gmail.com>
2021-02-15 17:55:30 +0000

    15/02

--- new_new2.py
+++ new_new2.py
@@ -4,8 +4,8 @@
 # git branch додавання гілки в репозиторій
 
 # git checkout  - перехід на іншу гілку git
-# git push -
-# git pull
-# git commit
-# git add
+# git push - закидання даних до віддаленого джерела - репозиторію
+# git pull - оновлення даних з джерела
+# git commit - закидання даних до ГІТ
+# git add - додавання файлу до ГІТ
 # git init, git merge ... name 'branch'
