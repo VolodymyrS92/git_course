42f80cc495be0ae9029f832d9b98adec94a6af19
Volodia <vovashishko253@gmail.com>
2021-02-15 17:45:22 +0000

    15-02

---
+++ new_new2.py
@@ -0,0 +1,11 @@
+# git command
+# git clone https/ клонування репозиторію
+
+# git branch додавання гілки в репозиторій
+
+# git checkout  - перехід на іншу гілку git
+# git push -
+# git pull
+# git commit
+# git add
+# git init, git merge ... name 'branch'
