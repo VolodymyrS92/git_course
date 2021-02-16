223298f804da21c42bcfc6307cd90da1716f1fb9
Volodia <vovashishko253@gmail.com>
2021-02-16 11:08:09 +0000

    16/02

--- new_new2.py
+++ new_new2.py
@@ -2,15 +2,14 @@
 # git clone https/ клонування репозиторію
 
 # git branch додавання гілки в репозиторій
-
+# git restore - видалення файлу з комміту гіта
 # git checkout  - перехід на іншу гілку git
 # git push - закидання даних до віддаленого джерела - репозиторію
 # git pull - оновлення даних з джерела
 # git commit - закидання даних до ГІТ
 # git add - додавання файлу до ГІТ
 # git init, git merge ... name 'branch'
-print('hello world')
-a = 3
-print (a)
-print('print')
+
+# git config global
+# .gitignore - ігнорування файлу гітом, прописуємо файл який ми хочемо заігнорити!!
 
